f57c98682043b40b4c9d8e3b46370b6dc6208824
Aleksandr Sher <aleksandrsher@gmail.com>
2013-10-17 13:45:26 +0400

    Added CLI parser.

--- src/pustota/core.clj
+++ src/pustota/core.clj
@@ -1,10 +1,26 @@
 (ns pustota.core
+  (use [clojure.tools.cli :only [cli]])
   (:gen-class))
 
 (defn plus "Sum of two numbers." [x y] (+ x y))
 
+
+(defn parse-args
+  "Парсит аргументы командной строки по заданному словарю."
+  [args]
+  (cli args
+       ["-w" "--workers" "Количество потоков" :parse-fn #(Integer. %) :default 2]
+       ["-r" "--report-period" "Частота отчетов (сек)" :parse-fn #(Integer. %) :default 10]
+       ["-d" "--dup-to-stop" "Количество дубликатов для остановки" :parse-fn #(Integer. %) :default 500]
+       ["-hf" "--hash-file" "Файл хешей" :default "hash.bin" ]
+       ["-qf" "--quotes-file" "Файл записей" :default "quotes.txt" ]
+       ["-h" "--help" "Show help" :default false :flag true]))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-
+  (let [[options args banner] (parse-args args)]
+    (when (:help options)
+      (println banner)
+      (System/exit 0))
+    (println options)))
