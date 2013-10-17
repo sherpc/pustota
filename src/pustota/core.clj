57541ea327706a32b220ab00eb18e3b14700e802
Aleksandr Sher <aleksandrsher@gmail.com>
2013-10-17 10:57:52 +0400

    Added easy test.

--- src/pustota/core.clj
+++ src/pustota/core.clj
@@ -1,7 +1,10 @@
 (ns pustota.core
   (:gen-class))
 
+(defn plus "Sum of two numbers." [x y] (+ x y))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
--- test/pustota/core_test.clj
+++ test/pustota/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [pustota.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest plus-test
+  (testing "sum of 1 and 2"
+    (is (= 3 (plus 1 2)))))
+
