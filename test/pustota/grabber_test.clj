2bfb67ce35fbe5abac5e0221d94c9a2725aca7e5
Aleksandr Sher <aleksandrsher@gmail.com>
2013-10-18 11:33:16 +0400

    Simplify of workers test.

--- test/pustota/grabber_test.clj
+++ test/pustota/grabber_test.clj
@@ -3,30 +3,20 @@
             [pustota.grabber :refer :all]
             [clojure.core.async :as async]))
 
-(defn reader
-  [result c]
-  (async/<!!
-    (async/go
-      (let [x (async/<! c)]
-        (println "*************** read from c: " x)
-        + result x))))
-
 (defn run-workers
   []
   (let [workers 4
         c (grab workers identity)]
-    (async/<!!
-     (async/go
-      (+ (async/<! c) (async/<! c) (async/<! c) (async/<! c))))))
-
-;;      (reduce
-;;       (fn [result _] (reader result grabber))
-;;       0
-;;       (range workers)))))
+       (reduce
+        (fn [r _] (+ r (async/<!! c)))
+        0
+        (range workers))))
 
 (deftest grab-test
   (testing "channel work"
     (is (= 6 (run-workers)))))
+
+
 
 
 
